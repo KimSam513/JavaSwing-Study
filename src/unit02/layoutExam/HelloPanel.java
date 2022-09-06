68b47628dfb560d7aee8e59971eee733a0205150
user <user@10.85.2.99>
2022-09-06 15:27:03 +0900

    기본예제

---
+++ src/unit02/layoutExam/HelloPanel.java
@@ -0,0 +1,9 @@
+package unit02.layoutExam;
+
+public class HelloPanel {
+
+	public static void main(String[] args) {
+		System.out.println("HelloPanel");
+	}
+
+}
---
+++ src/unit02/layoutExam/HelloSwing.java
@@ -0,0 +1,12 @@
+package unit02.layoutExam;
+
+public class HelloSwing {
+
+	//林籍贸府
+	public static void main(String[] args) {
+		System.out.println("HelloSwing");
+
+		//林籍贸府けけけけけ
+	}
+
+}
